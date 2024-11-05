2ba8e390adb3222bdbce86014235ff87ad627ab5
YashikaMandloi <145110078+YashikaMandloi@users.noreply.github.com>
2024-11-05 22:20:16 +0530

    Add files via upload

---
+++ server/controllers/authController.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const otpGenerator = require("otp-generator");
+const mailService = require("../services/mailer");
+const crypto = require("crypto");
+
+const filterObj = require("../utils/filterObj");
+
+// Model
+const User = require("../models/user");
+const otp = require("../Templates/Mail/otp");
+const resetPassword = require("../Templates/Mail/resetPassword");
+const { promisify } = require("util");
+const catchAsync = require("../utils/catchAsync");
+
+// this function will return you jwt token
+const signToken = (userId) => jwt.sign({ userId }, process.env.JWT_SECRET);
+
+// Register New User
